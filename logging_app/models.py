418cbe046ea2f3cf82bbda5379087d15b6ca8b9d
Blake9000 <blakealmackin@gmail.com>
2025-09-16 21:05:59 -0500

    Completed logging_app model

--- logging_app/models.py
+++ logging_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class LogSource(models.Model):
+    service_id = models.IntegerField()
+    source_type = models.CharField(max_length=16)
+    path = models.TextField() # Where the log comes from
+    parser = models.CharField(max_length=128, blank=True) # For future processing, what format is the log in?
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.source_type} {self.path}"
+
+
+class LogEvent(models.Model):
+    source = models.ForeignKey(LogSource, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    level = models.CharField(max_length=16) # error, info, warn, critical, etc
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{self.level}: {self.message}"
