b73260a70054c118cf19d576fdad9c71fa863837
Blake9000 <blakealmackin@gmail.com>
2025-09-16 21:34:36 -0500

    Created a basic framework for the user and user logging for now.

--- Assignement_Project_Blake_bmackin2/settings.py
+++ Assignement_Project_Blake_bmackin2/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'logging_app',
-    'service_dashboard',
+    'dashboard_view',
     'monitoring'
 ]
 
--- dashboard_view/models.py
+++ dashboard_view/models.py
@@ -3,3 +3,23 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=20)
+    password = models.TextField(null=True, blank=True, default=None, editable=False)
+    email = models.EmailField()
+    role = models.CharField(max_length=5) # Owner, admin, user
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+class LoginAudit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    username_attempted = models.CharField(max_length=128)
+    ip_address = models.GenericIPAddressField()
+    user_agent = models.TextField(blank=True)
+    success = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username_attempted}: {self.ip_address}"
+
+
