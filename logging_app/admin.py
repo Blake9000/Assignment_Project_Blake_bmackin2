df3a3945c38aa32c87101fc8c41bc6fb7b2e9deb
Blake9000 <blakealmackin@gmail.com>
2025-09-17 13:49:12 -0500

    Registered my models in the admin site.

--- logging_app/admin.py
+++ logging_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import LogSource, LogEvent
 # Register your models here.
+
+admin.site.register(LogSource)
+admin.site.register(LogEvent)
--- monitoring/admin.py
+++ monitoring/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Server,ServiceType, Service, Probe, CheckResult
 # Register your models here.
+
+
+admin.site.register(Server)
+admin.site.register(ServiceType)
+admin.site.register(Service)
+admin.site.register(Probe)
+admin.site.register(CheckResult)
