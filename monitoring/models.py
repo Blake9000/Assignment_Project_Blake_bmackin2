796dc1107494fecc200cddc957c03f2366f12d6c
Blake9000 <blakealmackin@gmail.com>
2025-09-16 20:58:41 -0500

    Completed monitoring model

--- monitoring/models.py
+++ monitoring/models.py
@@ -32,3 +32,30 @@ class Service(models.Model):
     def __str__(self):
         return f"{self.name} on {self.server.hostname}"
 
+class Probe(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='probes')
+    probe_type = models.CharField(max_length=16)  # Some types can be ping, TCP, HTTP, etc
+    target = models.CharField(max_length=512)     # Depends on the type of probe. For HTTP, it can be the URL, host:port for others
+    interval_seconds = models.PositiveIntegerField(default=30)      # Time between "Heartbeats"
+    timeout_seconds = models.PositiveIntegerField(default=5)        # Time before assuming some sort of connection error
+    enabled = models.BooleanField(default=True)
+    settings = models.JSONField(default=dict, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.probe_type} {self.target}"
+
+class CheckResult(models.Model):
+    probe = models.ForeignKey(Probe, on_delete=models.CASCADE, related_name='results')
+    timestamp = models.DateTimeField()
+    result = models.CharField(max_length=8) # Up, down
+    latency_ms = models.PositiveIntegerField(null=True, blank=True)
+    http_status = models.PositiveIntegerField(null=True, blank=True)
+    error = models.TextField(blank=True)
+    extra = models.JSONField(default=dict, blank=True)
+
+    def __str__(self):
+        return f"{self.probe} {self.result}"
+
+
+
