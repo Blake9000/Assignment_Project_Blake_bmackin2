6a2d37f0ce4bcba320331f4cb9896b0dafbcdaae
Blake9000 <blakealmackin@gmail.com>
2025-10-01 18:47:38 -0500

    Added basic search bar to the log viewer. Searches the message, level, hostname, and timestamp.

--- logging_app/views.py
+++ logging_app/views.py
@@ -2,13 +2,28 @@ from django.shortcuts import get_object_or_404, render
 from django.views import View
 from logging_app.models import LogEvent
 from django.views.generic import ListView
-
+from django.db.models import Q
 
 
 class LogEventsView(ListView):
     model = LogEvent
     context_object_name = 'log_events'
     template_name = "logging_app_view/logging_app_view.html"
+
+
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        q = self.request.GET.get('q')
+        if q:
+            search_qs = LogEvent.objects.filter(Q(message__icontains=q) | Q(level__icontains=q)|Q(source__service_id__server__hostname__icontains=q)|Q(timestamp__icontains=q))
+        else:
+            search_qs = LogEvent.objects.all()
+        ctx['log_events'] = search_qs
+        ctx['q'] = q
+        return ctx
+
+
 
 class LogDetailView(View):
 
