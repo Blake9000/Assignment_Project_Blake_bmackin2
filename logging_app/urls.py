92d465e3cf64c558314a94cad8ddac63d5b8d0a0
Blake9000 <blakealmackin@gmail.com>
2025-10-14 22:30:02 -0500

    Added dynamic chart to log viewer

--- logging_app/urls.py
+++ logging_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from logging_app.views import LogEventsView, LogDetailView
-
+from logging_app.views import errors_pie_chart
 urlpatterns = [
     path('logs/',LogEventsView.as_view(),
     name='logs'),
     path('logs/<int:primary_key>',LogDetailView.as_view(),
-         name='logs-detail')
+         name='logs-detail'),
+
+    path("logs/errors-pie.png", errors_pie_chart, name="errors-pie"),
+
 ]
--- logging_app/views.py
+++ logging_app/views.py
@@ -1,9 +1,11 @@
+from io import BytesIO
 from django.shortcuts import get_object_or_404, render
 from django.views import View
 from logging_app.models import LogEvent
 from django.views.generic import ListView
 from django.db.models import Q, Count
-
+import matplotlib.pyplot as plt
+from django.http import HttpResponse
 
 class LogEventsView(ListView):
     model = LogEvent
@@ -27,6 +29,12 @@ class LogEventsView(ListView):
             annotate(total=Count('level')).
             order_by('-total')
         )
+        ctx['sizes'] = []
+        ctx['labels'] = []
+        for eventCt in ctx['total_unique']:
+            ctx['sizes'].append(eventCt['total'])
+            ctx['labels'].append(eventCt['level'])
+
         return ctx
 
 
@@ -47,3 +55,39 @@ class LogDetailView(View):
                 'service': service
             }
         )
+
+
+
+def errors_pie_chart(request):
+    q = request.GET.get('q')
+    search_qs = LogEvent.objects.all()
+    if q:
+        search_qs = LogEvent.objects.filter(
+            Q(message__icontains=q) | Q(level__icontains=q) | Q(source__service_id__server__hostname__icontains=q) | Q(
+                timestamp__icontains=q))
+
+    total_unique = search_qs.values('level').annotate(total=Count('level'))
+    sizes = []
+    labels = []
+
+    for event in total_unique:
+        sizes.append(event['total'])
+        labels.append(event['level'])
+
+    if sum(sizes) == 0:
+        return HttpResponse()
+
+    fig, ax = plt.subplots(figsize=(10, 5), dpi=200)
+    ax.pie(sizes, labels=labels)
+    ax.set_title("Log Event Frequency")
+    fig.tight_layout()
+
+    buf = BytesIO()
+
+    fig.savefig(buf, format='png')
+
+    plt.close(fig)
+    buf.seek(0)
+
+
+    return HttpResponse(buf.getvalue(), content_type='image/png')
