0285109e0bc3310cf9fda02580b24ac42b82fd2a
Blake9000 <blakealmackin@gmail.com>
2025-10-22 18:10:47 -0500

    Created table view for servers. I will use roughly the same look for the rest of them.

--- dashboard_view/urls.py
+++ dashboard_view/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from dashboard_view.views import dashboard_login_view
+from dashboard_view.views import dashboard_login_view, AdminServers
 from dashboard_view.views import AdminView
 
 urlpatterns = [
     path('login/',dashboard_login_view, name='login'),
     path('admin/',AdminView.as_view(), name='admin'),
+
+    path('admin/servers',AdminServers.as_view(), name='admin-servers'),
 ]
--- dashboard_view/views.py
+++ dashboard_view/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from monitoring.models import Service, CheckResult, Server
 
 # Create your views here.
 
@@ -12,3 +13,9 @@ def dashboard_login_view(request):
 
 class AdminView(TemplateView):
     template_name = 'dashboard_view/admin_overview.html'
+
+
+class AdminServers(ListView):
+    model = Server
+    context_object_name = 'servers'
+    template_name = 'dashboard_view/admin_servers.html'
