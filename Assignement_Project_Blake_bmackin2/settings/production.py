fd3bd8aefe25a421f46116bd99348fcffc2b7712
Blake Mackin <bmackin2@illinois.edu>
2025-11-18 13:14:29 -0600

    updated allowed hosts, trying to host on cPanel

--- Assignement_Project_Blake_bmackin2/settings/production.py
+++ Assignement_Project_Blake_bmackin2/settings/production.py
@@ -4,4 +4,4 @@ DEBUG = False
 
 # Enter your PythonAnywhere account here in the allowed hosts.
 # Replace it with your name:
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bmackin2.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bmackin2.pythonanywhere.com', 'bmackin2.web.illinois.edu']
